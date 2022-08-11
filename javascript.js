af2340242f56feb24a66fa5d15bf3d61183b0e3f
Mattias Teesalu <mattiasteesalu@gmail.com>
2022-08-11 12:26:38 +0300

    Finished playRound function and made some minor visual changes in code

--- javascript.js
+++ javascript.js
@@ -5,11 +5,9 @@ function getComputerChoice() {
      let randomChoiceValue = Math.floor(Math.random() * 3);
      if (randomChoiceValue == "0") {
           return "Rock";
-     }
-     else if (randomChoiceValue == "1") {
+     } else if (randomChoiceValue == "1") {
           return "Paper";
-     }
-     else {
+     } else {
           return "Scissors";
      }
 }
@@ -20,13 +18,27 @@ function playRound(playerSelection, computerSelection) {
      if (playerSelection === 'Rock') {
           if (computerSelection === 'Paper') {
                log = 'You Lose! Paper beats Rock';
-          }
-          else if (computerSelection === 'Scissors') {
+          } else if (computerSelection === 'Scissors') {
                log = 'You Win! Rock beats Scissors';
-          }
-          else {
+          } else {
                log = "It's a tie";
           }
+     } else if (playerSelection === 'Paper') {
+          if (computerSelection === 'Scissors') {
+               log = 'You Lose! Scissors beats Paper';
+          } else if (computerSelection === 'Rock') {
+               log = 'You Win! Paper beats Rock';
+          } else {
+               log = "It's a tie";
+          }
+     } else if (playerSelection === 'Scissors') {
+          if (computerSelection === 'Rock') {
+               log = 'You Lose! Rock beats Scissors';
+          } else if (computerSelection === 'Paper') {
+               log = 'You Win! Scissors beats Paper';
+          } else {
+               log = "It's a tie";
+          } 
      }
      return log;
 }
