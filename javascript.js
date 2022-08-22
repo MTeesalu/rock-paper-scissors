d55b95f1cb6365fecb8b633161f9290f6652e31a
Mattias Teesalu <mattiasteesalu@gmail.com>
2022-08-22 12:58:04 +0300

    Made a game function

--- javascript.js
+++ javascript.js
@@ -1,44 +1,49 @@
-const computerSelection = getComputerChoice();
-const playerSelection = prompt();
-let yourScore = 0
+let playerSelection = prompt('Enter your choice');
+let computerSelection = getComputerChoice();
+let playerScore = 0
 let computerScore = 0
 
 function getComputerChoice() {
      let randomChoiceValue = Math.floor(Math.random() * 3);
-     if (randomChoiceValue == "0") {
-          return "rock";
-     } else if (randomChoiceValue == "1") {
-          return "paper";
+     if (randomChoiceValue == '0') {
+          return 'rock';
+     } else if (randomChoiceValue == '1') {
+          return 'paper';
      } else {
-          return "scissors";
+          return 'scissors';
      }
 }
 
 function playRound(playerSelection, computerSelection) {
      let log = '';
      playerSelection = playerSelection.toLowerCase();
-
      if (playerSelection === 'rock') {
           if (computerSelection === 'paper') {
                log = 'You Lose! Paper beats rock';
+               computerScore++;
           } else if (computerSelection === 'scissors') {
                log = 'You Win! Rock beats scissors';
+               playerScore++;
           } else {
                log = "It's a tie";
           }
      } else if (playerSelection === 'paper') {
           if (computerSelection === 'scissors') {
                log = 'You Lose! Scissors beats paper';
+               computerScore++;
           } else if (computerSelection === 'rock') {
                log = 'You Win! Paper beats rock';
+               playerScore++;
           } else {
                log = "It's a tie";
           }
      } else if (playerSelection === 'scissors') {
           if (computerSelection === 'rock') {
                log = 'You Lose! Rock beats scissors';
+               computerScore++;
           } else if (computerSelection === 'paper') {
                log = 'You Win! Scissors beats paper';
+               playerScore++;
           } else {
                log = "It's a tie";
           } 
@@ -46,6 +51,10 @@ function playRound(playerSelection, computerSelection) {
      return log;
 }
 
-function game(playRound) {
-
+function game() {
+     let outcome = playRound(playerSelection, computerSelection);
+     playerSelection = prompt('Enter your choice');
+     computerSelection = getComputerChoice();
+     playRound(playerSelection, computerSelection);
+     return outcome;
 }
