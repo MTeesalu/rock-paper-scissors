69d845d142b7531c1e632510b77b439d4d4c7edf
Mattias Teesalu <mattiasteesalu@gmail.com>
2022-09-03 14:02:07 +0300

    Made buttons for playerSelection

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let playerSelection;
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((button) => {
+     button.addEventListener('click', () => {
+          playerSelection = button.id;
+          playGame();
+     })
+})
+
 function getComputerChoice() {
      let randomChoiceValue = Math.floor(Math.random() * 3);
      if (randomChoiceValue == '0') {
@@ -49,40 +60,12 @@ function playRound(playerSelection, computerSelection) {
      return log;
 }
 
-for (let i = 0; i < 5; i++) {
-     function game() {
-          let playerSelection = prompt('Enter rock, paper or scissors :)');
-          let computerSelection = getComputerChoice();
-          let roundOutcome = playRound(playerSelection, computerSelection);          
-          console.log("Your score = " + playerScore);
-          console.log("Computer's score = " + computerScore);
-          console.log(roundOutcome);
-     }
-     if (i == 1) {
-          console.log('%c Round 1 Over', 'color: pink');
-     }
-     if (i == 2) {
-          console.log('%c Round 2 Over', 'color: pink');
-     }
-     if (i == 3) {
-          console.log('%c Round 3 Over', 'color: pink');
-     }
-     if (i == 4) {
-          console.log('%c Round 4 Over', 'color: pink');
-     }
-     
-     if (i !== 5 ) {
-          game();
+function playGame() {
+     let computerSelection = getComputerChoice();
+     let roundOutcome = playRound(playerSelection, computerSelection);          
+     console.log("Your score = " + playerScore);
+     console.log("Computer's score = " + computerScore);
+     console.log(roundOutcome);
      }     
-}
 
-if (playerScore > computerScore) {
-     console.log('%c Game Over! You won!', 'color: green');
-     alert('Good job! You won the game! Reload the page to play again :)');
-} else if (playerScore < computerScore) {
-     console.log('%c Game Over! You lost!', 'color: red');
-     alert('You lost! Better luck next time! Reload the page to play again :)');
-} else {
-     console.log('%c Game Over! Tie!', 'color: yellow');
-     alert("Unlucky! It's a tie! Reload the page to play again :)");
-     }
+
