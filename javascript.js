17e8bd76999ace750e2331948ecac421096aeaef
Mattias Teesalu <mattiasteesalu@gmail.com>
2022-09-26 19:13:59 +0300

    made reset button

--- javascript.js
+++ javascript.js
@@ -92,3 +92,11 @@ function playGame() {
           computer.textContent = `Computer Score: ${computerScore}`;
      }
 }    
+
+const refreshButton = document.querySelector('.refresh-button');
+
+const refreshPage = () => {
+  location.reload();
+}
+
+refreshButton.addEventListener('click', refreshPage)
